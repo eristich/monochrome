4ee34d16f1720ecd2ce5d53ef5f151000f6f7c6b
Esteban Ristich <esteban.ristich@protonmail.com>
2025-06-29 15:34:04 +0200

    feat: ajout de l'entité Music pour la database

---
+++ src/Entity/Music.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use App\Repository\MusicRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+use Doctrine\DBAL\Types\Types;
+
+#[ORM\Entity(repositoryClass: MusicRepository::class)]
+class Music
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    #[Groups(['music:get-one', 'music:get-collection'])]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['music:get-one', 'music:user-edit', 'music:get-collection'])]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'music')]
+    #[Groups(['music:get-one', 'music:get-collection'])]
+    private ?User $owner = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['music:get-one', 'music:user-edit', 'music:get-collection'])]
+    private ?string $artist = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['music:get-one', 'music:get-collection'])]
+    private ?string $originalFilename = null;
+
+    #[ORM\Column]
+    #[Groups(['music:get-one', 'music:get-collection'])]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column]
+    #[Groups(['music:get-one', 'music:get-collection'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $binaryFile = null;
+
+    #[ORM\Column(type: Types::BLOB, nullable: true)]
+    private $binaryCover = null;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+        $this->updatedAt = new \DateTimeImmutable();
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getArtist(): ?string
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(?string $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    public function getOriginalFilename(): ?string
+    {
+        return $this->originalFilename;
+    }
+
+    public function setOriginalFilename(string $originalFilename): static
+    {
+        $this->originalFilename = $originalFilename;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getBinaryFile()
+    {
+        return $this->binaryFile;
+    }
+
+    public function setBinaryFile($binaryFile): static
+    {
+        $this->binaryFile = $binaryFile;
+
+        return $this;
+    }
+
+    public function getBinaryCover()
+    {
+        return $this->binaryCover;
+    }
+
+    public function setBinaryCover($binaryCover): static
+    {
+        $this->binaryCover = $binaryCover;
+
+        return $this;
+    }
+}
