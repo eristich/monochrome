f6c141ce258fe85f4c1dd01144bda61558614f36
Esteban Ristich <esteban.ristich@protonmail.com>
2025-07-01 20:06:41 +0200

    feat: ajoute un fichier de configuration pour le code quality

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::PSR_12,
+        SetList::SYMPLIFY,
+        SetList::COMMON,
+        SetList::SPACES,
+        SetList::NAMESPACES,
+        SetList::COMMENTS
+    ]);
+
+    $ecsConfig->cacheDirectory(__DIR__ . '/var/cache/ecs');
+};
