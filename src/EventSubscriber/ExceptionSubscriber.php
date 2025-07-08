f2261d0b23ac8eaf89771e5b34bfa1891970947f
Esteban Ristich <esteban.ristich@protonmail.com>
2025-07-08 17:27:54 +0200

    feat: gestion des erreurs de l'api au format JSON

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response = new JsonResponse([
+                'error' => $exception->getMessage(),
+            ], $exception->getStatusCode());
+        } else {
+            $response = new JsonResponse([
+                'error' => 'Erreur interne du serveur',
+            ], 500);
+        }
+
+        $event->setResponse($response);
+    }
+}
