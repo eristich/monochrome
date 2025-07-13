086b5157f17be87ab6f81d93d11a34254efc634b
Esteban Ristich <esteban.ristich@protonmail.com>
2025-07-14 00:35:40 +0200

    feat: ajout du service MessageService pour l'envoi de messages via Mercure
    Issues: #21

---
+++ src/Service/MessageService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+use App\Dto\MessagePayloadDto;
+use App\Entity\User;
+
+class MessageService
+{
+    public function __construct(
+        private HubInterface $hub,
+        private SerializerInterface $serializer
+    ) {}
+
+    public function sendMessage(User $user, string $content): MessagePayloadDto
+    {
+        // Créer le DTO du message
+        $messageData = new MessagePayloadDto();
+        $messageData->content = $content;
+        $messageData->userId = $user->getId()->__toString();
+        $messageData->userName = $user->getName();
+
+        // Sérialiser le message pour Mercure
+        $messageJson = $this->serializer->serialize($messageData, 'json', ['groups' => ['message:over-sse']]);
+
+        // Créer et publier l'update Mercure
+        $update = new Update(
+            'message', // Topic
+            $messageJson,
+            false
+        );
+
+        $this->hub->publish($update);
+
+        return $messageData;
+    }
+}
