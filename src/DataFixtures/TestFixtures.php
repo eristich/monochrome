113f07916465952cbc76587c3f303c53e61a6376
Esteban Ristich <esteban.ristich@protonmail.com>
2025-07-13 19:29:43 +0200

    chore: jeu de donnée test pour les stats de diffusion

--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -9,6 +9,7 @@ use Symfony\Component\Uid\Uuid;
 use App\Factory\UserFactory;
 use App\Entity\Music;
 use App\Entity\User;
+use App\Entity\StatDiffusion;
 
 class TestFixtures extends Fixture implements FixtureGroupInterface
 {
@@ -82,6 +83,49 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
                 ->setOwner($manager->getRepository(User::class)->find(Uuid::fromString('0197bc57-124c-747d-b7a3-20eec51be0b2')))
                 ->setBinaryFile(file_get_contents(dirname(__DIR__, 2) . '/music-fixtures/' . $metadata['originalFilename']));
             $manager->persist($music);
+        }
+
+        // Statistiques de diffusion fixtures
+        $statsData = [
+            [
+                'name' => 'Chameleon',
+                'artist' => 'Hicham Chahidi',
+                'duration' => 180,
+                'createdAt' => new \DateTimeImmutable('2024-01-15 10:30:00')
+            ],
+            [
+                'name' => 'NIGHT',
+                'artist' => 'KOSMORIDER',
+                'duration' => 240,
+                'createdAt' => new \DateTimeImmutable('2024-01-16 14:45:00')
+            ],
+            [
+                'name' => 'Chameleon',
+                'artist' => 'Hicham Chahidi',
+                'duration' => 180,
+                'createdAt' => new \DateTimeImmutable('2024-01-17 09:15:00')
+            ],
+            [
+                'name' => 'NIGHT',
+                'artist' => 'KOSMORIDER',
+                'duration' => 240,
+                'createdAt' => new \DateTimeImmutable('2024-01-20 16:20:00')
+            ],
+            [
+                'name' => 'Chameleon',
+                'artist' => 'Hicham Chahidi',
+                'duration' => 180,
+                'createdAt' => new \DateTimeImmutable('2024-01-25 11:00:00')
+            ]
+        ];
+
+        foreach ($statsData as $statData) {
+            $stat = new StatDiffusion();
+            $stat->setName($statData['name'])
+                ->setArtist($statData['artist'])
+                ->setDuration($statData['duration'])
+                ->setCreatedAt($statData['createdAt']);
+            $manager->persist($stat);
         }
 
         $manager->flush();
