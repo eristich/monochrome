d2ca277a40c6c8cf1a71b419da53d23b69fe38fe
Esteban Ristich <esteban.ristich@protonmail.com>
2025-07-27 19:49:56 +0200

    feat: ajout du logo svg Monochrome en composant React

---
+++ frontend/src/component/nano/ui-monochrome-icon.tsx
@@ -0,0 +1,23 @@
+export default function UIMonochromeIcon() {
+  return (
+    <svg width="91" height="94" viewBox="0 0 91 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_116_672)">
+      <path d="M39.4972 47C39.4972 72.9574 73.7606 94 47.5174 94C21.2743 94 0 72.9574 0 47C0 21.0426 21.2743 0 47.5174 0C73.7606 0 39.4972 21.0426 39.4972 47Z" fill="url(#paint0_linear_116_672)"/>
+      <circle cx="72.0954" cy="47.0862" r="18.9725" fill="url(#paint1_linear_116_672)"/>
+      </g>
+      <defs>
+      <linearGradient id="paint0_linear_116_672" x1="86.9284" y1="7.33027" x2="28.706" y2="94" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#CBCBCB"/>
+      <stop offset="1"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear_116_672" x1="101.158" y1="31.9945" x2="63.5578" y2="62.9541" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#545454"/>
+      <stop offset="1" stopColor="#999999"/>
+      </linearGradient>
+      <clipPath id="clip0_116_672">
+      <rect width="91" height="94" fill="white"/>
+      </clipPath>
+      </defs>
+    </svg>
+  )
+}
